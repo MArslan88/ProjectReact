407b1ccfe0c166f944bd9adc22a075eb380be5b9
MArslan88 <gery_maz_555@hotmail.com>
2021-08-06 23:38:42 +0500

    Name and status update with state

--- src/App.js
+++ src/App.js
@@ -2,21 +2,17 @@ import React, {useState} from 'react';
 import Tweet from './Tweet';
 
 function App(){
-
-  const [isRed, setRed] = useState(false);
-  const [count, setCount] = useState(0);
-
-
-  const increment = () => {
-    setCount(count+1);
-    setRed(!isRed);
-  };
+  const [users, setUsers] = useState([
+    {name: "Arslan", message: "Hello there", like: "2"},
+    {name: "Danish", message: "I am Danish Nisar", like: "1"},
+    {name: "Tanzeel", message: "I am awesome", like: "5"}
+  ]);
 
     return(
     <div className="app">
-      <h1 className={isRed ? "red" : ""}>Change my colour!</h1>
-      <button onClick={increment}>Increment</button>
-      <h1>{count}</h1>
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message} like={user.like} />
+      ))}
     </div>
   );
 }
