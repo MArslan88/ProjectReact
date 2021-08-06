50979dabb4c63267e3255bf3c4039e1d87423290
MArslan88 <gery_maz_555@hotmail.com>
2021-08-06 09:54:22 +0500

    Tweet-app update

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ function App(){
 
     return(
     <div className="app">
-      <h1>This is the app component</h1>
-      <Tweet />
+      <Tweet name="Arsal" message="This is a random tweet" like="2"/>
+      <Tweet name="Tanzeel" message="I am the true king" like="4"/>
+      <Tweet name="Danish" message="50k my dues" like="3"/>
+      <Tweet name="Noman" message="My new course is available" like="1"/>
     </div>
   );
 }
--- src/Tweet.js
+++ src/Tweet.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css'
 
-function Tweet(){
+// First method with the help of PROPS----------------------------
+// function Tweet(props){
+//     return(
+//         <div className="tweet">
+//             <h3>{props.name}</h3>
+//             <p>{props.message}</p>
+//             <h3>{props.like}</h3>
+//         </div>
+//     );
+// }
+
+// Second method with the help of peremeters----------------------------
+
+function Tweet({name,message,like}){
     return(
         <div className="tweet">
-            <h3>Name</h3>
-            <p>This is random tweet</p>
-            <h3>Number of likes</h3>
+            <h3>{name}</h3>
+            <p>{message}</p>
+            <h3>{like}</h3>
         </div>
     );
 }
